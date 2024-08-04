8add81b1448311eb9bd5d1f5ba795236f92f77e4
FatherofElias <supra_3_8@hotmail.com>
2024-08-04 15:17:16 -0500

    update q1t1.2

---
+++ budget_category.py
@@ -0,0 +1,33 @@
+# Task 1
+
+# this is the module for task 1 with the private attributes (category name, allocated budget, remaining budget)
+
+
+class BudgetCategory:
+    def __init__(self, category_name, allocated_budget):
+        self.__category_name = category_name
+        self.__allocated_budget = allocated_budget
+        self.__remaining_budget = allocated_budget
+
+
+# Task 2 
+
+     # Getter for category name
+    def get_category_name(self):
+        return self.__category_name
+
+    # Setter for category name
+    def set_category_name(self, category_name):
+        self.__category_name = category_name
+
+    # Getter for allocated budget
+    def get_allocated_budget(self):
+        return self.__allocated_budget
+
+    # Setter for allocated budget with validation
+    def set_allocated_budget(self, allocated_budget):
+        if allocated_budget > 0:
+            self.__allocated_budget = allocated_budget
+            self.__remaining_budget = allocated_budget
+        else:
+            raise ValueError("Budget must be a positive number")
